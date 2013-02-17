66ebe52eb4312559e0160f7d9b44d92fa1b6c837
Wilfred Hughes <me@wilfred.me.uk>
2013-02-17 21:05:58 +0000

    expanding test coverage

--- ht-tests.el
+++ ht-tests.el
@@ -6,6 +6,16 @@
     (ht-set h "foo" "bar")
     (should (equal (ht-keys h) (list "foo")))))
 
+(ert-deftest ht-test-values ()
+  (let ((h (ht-create)))
+    (ht-set h "foo" "bar")
+    (should (equal (ht-values h) (list "bar")))))
+
+(ert-deftest ht-test-items ()
+  (let ((h (ht-create)))
+    (ht-set h "key1" "value1")
+    (should (equal (sort (ht-items h) '<) '(("key1" "value1"))))))
+
 (defun ht-run-tests ()
   (interactive)
   (ert-run-tests-interactively "ht-test-"))
